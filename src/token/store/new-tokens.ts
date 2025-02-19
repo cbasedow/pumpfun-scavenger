8b636063875bc54bd2703c82226b7939042d0f48
Charles J Basedow <charlesjbasedow@proton.me>
2025-02-19 10:57:11 -0500

    add random buyable age between 30-60 secs for new tokens

--- src/token/store/new-tokens.ts
+++ src/token/store/new-tokens.ts
@@ -4,8 +4,20 @@ import { type ResultAsync, errAsync, fromPromise, okAsync } from "neverthrow";
 import type { NewToken } from "../types";
 
 export const NEW_TOKENS_KEY = "new:tokens";
-const NEW_TOKENS_MAX_AGE_MS = 60 * 60 * 1000; // New tokens max age is 1 hour
-const BUYABLE_AGE_MS = 2 * 60 * 1000; // New tokens are eligible for buying after 2 minutes
+const NEW_TOKENS_MAX_AGE_MS = 10 * 60 * 1000; // New tokens max age is 10 minutes
+
+const MIN_BUYABLE_AGE_MS = 30 * 1000; // Min buyable age is 30 seconds
+const MAX_BUYABLE_AGE_MS = 60 * 1000; // Max buyable age is 1 minute
+const BUYABLE_AGE_RANGE_MS = MAX_BUYABLE_AGE_MS - MIN_BUYABLE_AGE_MS;
+
+/**
+ * Gets a random buyable age in milliseconds between MIN_BUYABLE_AGE_MS and MAX_BUYABLE_AGE_MS
+ * @returns number
+ */
+const getRandomBuyableAgeMs = (): number => {
+	const randomInt = Math.floor(Math.random() * BUYABLE_AGE_RANGE_MS);
+	return randomInt + MIN_BUYABLE_AGE_MS;
+};
 
 /**
  * Adds a new token to the redis new tokens zset
@@ -32,14 +44,14 @@ const addToken = (newToken: NewToken): ResultAsync<number, Error> => {
 };
 
 /**
- * Gets all the new tokens with a score greater than 2 minutes ago and less than 1 hour ago
+ * Gets all the new tokens with a score between 30-60 seconds ago and less than 1 hour ago
  * @returns ResultAsync<NewToken[], Error>
  */
 const getTokensEligibleForBuying = (): ResultAsync<NewToken[], Error> => {
 	const now = Date.now();
 
-	const minBuyableAge = now - BUYABLE_AGE_MS; // 2 minutes ago
-	const maxNewTokenAge = now - NEW_TOKENS_MAX_AGE_MS; // 1 hour ago
+	const minBuyableAge = now - getRandomBuyableAgeMs(); // Min buyable age (anywhere between 30 secs and 1 minute)
+	const maxNewTokenAge = now - NEW_TOKENS_MAX_AGE_MS; // Maxc age of the new tokens (10 minutes)
 
 	return fromPromise(
 		redis.zrange<NewToken[]>(NEW_TOKENS_KEY, maxNewTokenAge, minBuyableAge, {
