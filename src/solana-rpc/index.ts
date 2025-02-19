e9a57ad3badf7eb30003f7a16825ce2a316f53b6
Charles J Basedow <charlesjbasedow@proton.me>
2025-02-19 09:54:00 -0500

    add sendTransactionWithRetries solana rpc util

--- src/solana-rpc/index.ts
+++ src/solana-rpc/index.ts
@@ -1,7 +1,9 @@
 import { connection } from "./connection";
 import { getAccountInfo } from "./methods/get-account-info";
+import { sendTransactionWithRetries } from "./utils/send-transaction-with-retries";
 
 export const solanaRpc = {
 	connection,
 	getAccountInfo,
+	sendTransactionWithRetries,
 } as const;
